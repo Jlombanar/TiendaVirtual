1439d2e5604fd85896188ececb261181adba790a
Jlombanar <jalmpa77@gmail.com>
2025-12-05 07:38:25 -0500

    actualizando servicios de correo

--- backend/controllers/recuperar.js
+++ backend/controllers/recuperar.js
@@ -13,11 +13,14 @@ if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
 // CONFIGURACIÓN DE CORREO
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+    },
+    tls: {
+        rejectUnauthorized: false
     },
     connectionTimeout: 10000,
     greetingTimeout: 10000,
