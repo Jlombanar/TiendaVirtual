8763a522c2fef1144dc6c0fe2732f14d58f2a827
Jlombanar <jalmpa77@gmail.com>
2025-12-04 22:40:21 -0500

    actualizandolos etornos

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const corsOptions = {
     'http://localhost:3000',
     'http://localhost:5173'
   ],
-  credentials: true,
+ 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
